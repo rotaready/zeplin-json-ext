d7597313713f1eb44d368013bb2f46ba56cff1d0
flouc001 <flouc001@gmail.com>
2018-09-11 14:57:40 +0100

    linting, editorconfig

--- src/index.js
+++ src/index.js
@@ -1,129 +1,141 @@
 const colorToRGB = (color, includeAlpha = false) => {
-    const {r, g, b, a} = color;
+  const {
+    r,
+    g,
+    b,
+    a,
+  } = color;
 
-    if (includeAlpha) {
-        return `rgba(${r}, ${g}, ${b}, ${a})`;
-    }
+  if (includeAlpha) {
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+  }
 
-    return `rgb(${r}, ${g}, ${b})`;
+  return `rgb(${r}, ${g}, ${b})`;
 };
 
-const colorToHex = color => {
-    const {r, g, b} = color.toHex();
-    return `#${r}${g}${b}`;
-}
+const colorToHex = (color) => {
+  const { r, g, b } = color.toHex();
+
+  return `#${r}${g}${b}`;
+};
 
 const formatColor = (option, color) => {
-    let outputColor;
+  let outputColor;
 
-    switch (option) {
-        case "rgb":
-            outputColor = colorToRGB(color);
-            break;
-        case "rgba":
-            outputColor = colorToRGB(color, true);
-            break;
-        case "hex":
-        default:
-            outputColor = colorToHex(color);
-            break;
-    }
+  switch (option) {
+    case 'rgb':
+      outputColor = colorToRGB(color);
+      break;
+    case 'rgba':
+      outputColor = colorToRGB(color, true);
+      break;
+    case 'hex':
+    default:
+      outputColor = colorToHex(color);
+      break;
+  }
 
-    return outputColor;
-}
-
-const buildColorJSON = (context, colors) => {
-    const output = {};
-
-    const colorOption = context.getOption('color-format');
-    const formatColorWithOption = formatColor.bind(this, colorOption);
-
-    colors.forEach((color) => {
-        output[color.name] = formatColorWithOption(color);
-    });
-
-    return output;
-}
-
-const buildTextStyleJSON = (context, textStyles) => {
-    const output = {};
-
-    const fontFamilyOption = context.getOption('include-font-family');
-    const fontSizeOption = context.getOption('include-font-size');
-    const fontWeightOption = context.getOption('include-font-weight');
-    const fontColorOption = context.getOption('include-font-color');
-
-    const colorOption = context.getOption('color-format');
-    const formatColorWithOption = formatColor.bind(this, colorOption);
-
-    if (!(fontFamilyOption || fontSizeOption || fontWeightOption || fontColorOption)) {
-        // no point in looping
-        return output;
-    }
-
-    textStyles.forEach(({ name, fontFamily, fontSize, fontWeight, color }) => {
-        output[name] = {};
-
-        if (fontFamilyOption) output[name].fontFamily = fontFamily;
-        if (fontSizeOption) output[name].fontSize = fontSize;
-        if (fontWeightOption) output[name].weight = fontWeight;
-        if (fontColorOption) output[name].color = formatColorWithOption(color);
-    });
-
-    return output;
+  return outputColor;
 };
 
-function layer(context, selectedLayer) {
+const buildColorJSON = (context, colors) => {
+  const output = {};
+
+  const colorOption = context.getOption('color-format');
+  const formatColorWithOption = formatColor.bind(this, colorOption);
+
+  colors.forEach((color) => {
+    output[color.name] = formatColorWithOption(color);
+  });
+
+  return output;
+};
+
+const buildTextStyleJSON = (context, textStyles) => {
+  const output = {};
+
+  const fontFamilyOption = context.getOption('include-font-family');
+  const fontSizeOption = context.getOption('include-font-size');
+  const fontWeightOption = context.getOption('include-font-weight');
+  const fontColorOption = context.getOption('include-font-color');
+
+  const colorOption = context.getOption('color-format');
+  const formatColorWithOption = formatColor.bind(this, colorOption);
+
+  if (!(fontFamilyOption || fontSizeOption || fontWeightOption || fontColorOption)) {
+    // no point in looping
+    return output;
+  }
+
+  textStyles.forEach(({
+    name,
+    fontFamily,
+    fontSize,
+    fontWeight,
+    color,
+  }) => {
+    output[name] = {};
+
+    if (fontFamilyOption) output[name].fontFamily = fontFamily;
+    if (fontSizeOption) output[name].fontSize = fontSize;
+    if (fontWeightOption) output[name].weight = fontWeight;
+    if (fontColorOption) output[name].color = formatColorWithOption(color);
+  });
+
+  return output;
+};
+
+function layer() {
 
 }
 
 function styleguideColors(context, colors) {
-    const output = buildColorJSON(context, colors);
+  const output = buildColorJSON(context, colors);
 
-    return {
-        code: JSON.stringify(output, null, 2),
-        language: 'json',
-    };
+  return {
+    code: JSON.stringify(output, null, 2),
+    language: 'json',
+  };
 }
 
 function styleguideTextStyles(context, textStyles) {
-    const output = buildTextStyleJSON(context, textStyles);
+  const output = buildTextStyleJSON(context, textStyles);
 
-    return {
-        code: JSON.stringify(output, null, 2),
-        language: 'json',
-    };
+  return {
+    code: JSON.stringify(output, null, 2),
+    language: 'json',
+  };
 }
 
 function exportStyleguideColors(context, colors) {
-    const output = buildColorJSON(context, colors);
+  const output = buildColorJSON(context, colors);
 
-    return {
-        code: JSON.stringify(output, null, 2),
-        language: 'json',
-        filename: 'colors.json',
-    };
+  return {
+    code: JSON.stringify(output, null, 2),
+    language: 'json',
+    filename: 'colors.json',
+  };
 }
 
 function exportStyleguideTextStyles(context, textStyles) {
-    const output = buildTextStyleJSON(context, textStyles);
+  const output = buildTextStyleJSON(context, textStyles);
 
-    return {
-        code: JSON.stringify(output, null, 2),
-        language: 'json',
-        filename: 'fonts.json',
-    };
+  return {
+    code: JSON.stringify(output, null, 2),
+    language: 'json',
+    filename: 'fonts.json',
+  };
 }
 
-function comment(context, text) {
+function comment() {
 
 }
 
 export default {
-    layer,
-    styleguideColors,
-    styleguideTextStyles,
-    exportStyleguideColors,
-    exportStyleguideTextStyles,
-    comment
+  layer,
+  styleguideColors,
+  styleguideTextStyles,
+  exportStyleguideColors,
+  exportStyleguideTextStyles,
+  comment,
 };
